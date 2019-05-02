ec65766b4be8eea48ab0c184abc811016f704f8b
James de Winton <james.dewinton@gmail.com>
2019-05-02 11:40:53 +0100

    fizz and buzz and fizzbuzz

--- fizzbuzz/fizzbuzz.py
+++ fizzbuzz/fizzbuzz.py
@@ -1,3 +1,9 @@
 def fizzbuzz(number):
-    if number % 3 == 0:
-        return "fizzbuzz"
+    x = ""
+    if number % 3 == 0 and number % 5 == 0:
+        x = x + "fizzbuzz"
+    elif number % 3 == 0:
+        x = x + "fizz"
+    elif number % 5 == 0:
+        x = x + "buzz"
+    return x
--- fizzbuzz/test_fizzbuzz.py
+++ fizzbuzz/test_fizzbuzz.py
@@ -2,8 +2,11 @@ import unittest
 from fizzbuzz import fizzbuzz
 
 class TestFizzBuzz(unittest.TestCase):
-    def test_fizzbuzz(self):
-        self.assertEqual(fizzbuzz(3), "fizzbuzz")
+    def test_fizz(self):
+        self.assertEqual(fizzbuzz(3), "fizz")
+    def test_buzz(self):
+        self.assertEqual(fizzbuzz(5), "buzz")
+
 
 
 if __name__ == '__main__':
